457bdf4b34343be852c553efed7f164738e90642
mudit <mksingh458@gmail.com>
2020-08-15 11:17:42 +0530

    added pbds.cpp

---
+++ template/pbds.cpp
@@ -0,0 +1,17 @@
+
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+//#pragma GCC optimize ("-O3")
+
+using namespace std;
+//mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+//const long double PI = 3.14159265358979323846;
+
+typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> indexed_set;/* find_by_order() order_of_key() */
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+    return 0;
+}
